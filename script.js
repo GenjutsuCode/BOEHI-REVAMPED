fc8aa06035a6ef3b1ad83c8c9f09dddcce308264
Adebola Ayeni <adebolaaaaaa@gmail.com>
2026-05-13 16:41:05 +0100

    added 2 videos

--- script.js
+++ script.js
@@ -3,27 +3,6 @@ function toggleMenu() {
     document.querySelector('.menu-overlay').classList.toggle('active');
 }
 
-function postPreview(btn) {
-    const key = btn.getAttribute("postkey");
-    const post = posts[key];
-
-    if (!post) {
-        console.warn(`No post found for key: "${key}"`);
-        return;
-    }
-
-    document.querySelector(".post-preview-title").textContent = post.title;
-    document.querySelector(".post-preview-date").textContent = post.date;
-    document.querySelector(".post-preview-content").innerHTML = post.content;
-    document.querySelector('.post-preview').classList.toggle('active');
-    document.querySelector('.post-preview').scrollTop = 0;
-
-}
-
-function closePreview(){
-    document.querySelector('.post-preview').classList.toggle('active');
-}
-
 new Typed('#typed', {
   strings: [
     'education and impact.',
